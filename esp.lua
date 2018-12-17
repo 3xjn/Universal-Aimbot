55ef128a6662a17948c79776f6f77cbc88712a68
Tesseracting <mcphersonasher@gmail.com>
2018-12-16 21:45:35 -0500

    Revert "Esp v0.2"
    This reverts commit 1d6802306a1e2dfb01f171c6e47e02a3e0c68a17.

--- esp.lua
+++ esp.lua
@@ -2,7 +2,7 @@ while wait() do
     local players = game.Players:GetPlayers()
     local LocalPlayer = game.Players.LocalPlayer 
     for i=1, #players do
-		if workspace:FindFirstChild(players[i].Name) and players[i].Name ~= LocalPlayer.Name then
+		if players[i].TeamColor == LocalPlayer.TeamColor and workspace:FindFirstChild(players[i].Name) and players[i].Name ~= LocalPlayer.Name then
             local BillboardGui = Instance.new("BillboardGui")
 			local Frame = Instance.new("Frame")
 			--Properties:
@@ -15,6 +15,20 @@ while wait() do
 			Frame.Parent = BillboardGui
 			Frame.BackgroundColor3 = Color3.new(players[i].TeamColor.r, players[i].TeamColor.g, players[i].TeamColor.b)
 			Frame.Size = UDim2.new(0, 10, 0, 10)
-		end
+			end
+		if players[i].TeamColor ~= LocalPlayer.TeamColor and workspace:FindFirstChild(players[i].Name) and players[i].Name ~= LocalPlayer.Name then
+			local BillboardGui = Instance.new("BillboardGui")
+			local Frame = Instance.new("Frame")
+			--Properties:
+			BillboardGui.Parent = game.Workspace[players[i].Name].Head
+			BillboardGui.Active = true
+			BillboardGui.AlwaysOnTop = true
+			BillboardGui.LightInfluence = 0
+			BillboardGui.Size = UDim2.new(0, 20, 0, 20)
+			
+			Frame.Parent = BillboardGui
+			Frame.BackgroundColor3 = Color3.new(players[i].TeamColor.r, players[i].TeamColor.g, players[i].TeamColor.b)
+			Frame.Size = UDim2.new(0, 10, 0, 10)
+        end
     end
 end
